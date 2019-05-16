3338a7ef71e34488449fe7bcd14e0c88b99969fe
Erin Moon <erin@hashbang.sh>
2019-05-16 14:39:04 -0500

    checkin current work

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,114 @@
-fn main() {
-    println!("Hello, world!");
+use env_logger;
+use exitfailure::ExitFailure;
+use failure::Error;
+use failure::{self, ResultExt};
+use htmlescape;
+use std::path::PathBuf;
+
+use log::{debug, info, warn};
+use reqwest;
+use serde::Deserialize;
+use serde_json::{json, Value};
+use structopt::StructOpt;
+use url::Url;
+
+#[derive(Debug, StructOpt)]
+struct Opts {
+    /// Path where fetchmixes will put its Sqlite database. If not present, defaults to `./fetchmixes.db`.
+    #[structopt(short = "d", long = "database", parse(from_os_str))]
+    db_path: Option<PathBuf>,
+
+    #[structopt(subcommand)]
+    cmd: Command,
+}
+
+#[derive(Debug, StructOpt)]
+enum Command {
+    /// Crawl a DJ's page on Mixcloud. Just saves a list of sets in the fetchmixes database; does _not_ download audio or setlists.
+    #[structopt(name = "crawl-dj")]
+    CrawlDj { dj_username: String },
+}
+
+fn get_csrf_token(client: &reqwest::Client, profile_url: &str) -> Result<String, Error> {
+    let resp = client.get(profile_url).send()?;
+    let csrfcookie = resp.cookies().find(|c| c.name() == "csrftoken").unwrap();
+    Ok(csrfcookie.value().to_string())
+}
+
+#[derive(Debug, Deserialize)]
+struct CloudcastCovers {
+    extra_large: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct Cloudcast {
+    play_count: i64,
+    url: String,
+    pictures: CloudcastCovers,
+}
+
+fn main() -> Result<(), ExitFailure> {
+    env_logger::init();
+    let opts = Opts::from_args();
+    match opts.cmd {
+        Command::CrawlDj { dj_username } => {
+            let frontend_base = Url::parse("https://www.mixcloud.com/")?;
+            let api_base = Url::parse("https://api.mixcloud.com/")?;
+            let graphql_endpoint = Url::parse("https://www.mixcloud.com/graphql")?;
+
+            let profile_page = frontend_base.join(&dj_username)?;
+            let api_page = api_base.join(&dj_username)?;
+
+            let client = reqwest::Client::new();
+
+            // -- get a CSRF token
+            let csrf = get_csrf_token(&client, profile_page.as_str())?;
+            info!("Got CSRF token: {}", csrf);
+
+            // -- get mixcount
+            let dj_info: Value = client.get(api_page.as_str()).send()?.json()?;
+            let n_mixes = dj_info["cloudcast_count"].as_i64().unwrap();
+
+            info!("Attempting to fetch {} mixes", n_mixes);
+
+            // -- get ID from UserStatsCard
+            let payload_userstatscard = json! {
+                {"id":"q59","query":"query UserStatsCard($lookup_0:UserLookup!,$first_1:Int!) {userLookup(lookup:$lookup_0) {id,...F1}} fragment F0 on Stats {comments {totalCount},favorites {totalCount},reposts {totalCount},plays {totalCount},minutes {totalCount},__typename} fragment F1 on User {isViewer,isUploader,username,hasProFeatures,_uploads1No11a:uploads(first:$first_1) {edges {node {id},cursor},pageInfo {hasNextPage,hasPreviousPage}},stats {...F0},id}","variables":{"lookup_0":{"username":"Grundfunk"},"first_1":1}}
+            };
+            let usercard: Value = client
+                .post(graphql_endpoint.as_str())
+                .header("X-CSRFToken", csrf.as_str())
+                .header("Referer", profile_page.as_str())
+                .header("Cookie", format!("csrftoken={}", csrf))
+                .json(&payload_userstatscard)
+                .send()?
+                .json()?;
+
+            let dj_id = usercard["data"]["userLookup"]["id"].as_str().unwrap();
+            info!("DJ has internal user id of: {}", dj_id);
+
+            let mut cloudcasts: Vec<Cloudcast> = Vec::new();
+            let mut cloudcasts_api = api_page.clone();
+            cloudcasts_api
+                .path_segments_mut()
+                .unwrap()
+                .push("cloudcasts");
+            loop {
+                debug!("fetching url {}", cloudcasts_api.as_str());
+                let resp: Value = client.get(cloudcasts_api.as_str()).send()?.json()?;
+                let ccast_list: Vec<Cloudcast> = serde_json::from_value(resp["data"].clone())?;
+                cloudcasts.extend(ccast_list);
+
+                match resp["paging"]["next"].as_str() {
+                    Some(next_page) => cloudcasts_api = Url::parse(next_page)?,
+                    None => {
+                        debug!("hit end of cloudcasts list pagination, breaking");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    Ok(())
 }
